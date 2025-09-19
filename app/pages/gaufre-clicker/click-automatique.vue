444bda72a8696c91246f5c8fbf528fb25000b9b0
Thomasn9 <thciccaredev@gmail.com>
2025-09-19 12:19:27 +0200

    auto click améliorer

--- app/pages/gaufre-clicker/click-automatique.vue
+++ app/pages/gaufre-clicker/click-automatique.vue
@@ -22,10 +22,12 @@ import { ref, onMounted, onUnmounted } from 'vue'
 
 const cumul = ref(0)
 const valeurClick = ref(1)
+
 const prixAmelioration1 = ref(5)
 const prixAmelioration2 = ref(20)
+
 const valeurAutoClick = ref(1)
-const valeurAmelioration2 = ref(null)
+const valeurAmelioration2 = ref(5)
 
 function augmentation(){
     cumul.value += valeurClick.value;
@@ -43,26 +45,20 @@ function amelioration1(){
 function amelioration2(){
     if (cumul.value >= prixAmelioration2.value) {
         cumul.value -= prixAmelioration2.value;
-        prixAmelioration2.value = Math.floor(prixAmelioration2.value * 1.8); 
+        prixAmelioration2.value = Math.floor(prixAmelioration2.value * 1.5); 
         valeurAutoClick.value += 1;
-        
-        // alumage du click
-        if (!valeurAmelioration2.value) {
-            startAutoClick();
+
+        if (valeurAmelioration2.value) {
+        clearInterval(valeurAmelioration2.value);
         }
+        
+        //vitesse 
+        valeurAmelioration2.value = setInterval(() => {
+            cumul.value += valeurAutoClick.value;
+        }, 1000);
     }
 }
 
-function startAutoClick() {
-    if (valeurAmelioration2.value) {
-        clearInterval(valeurAmelioration2.value);
-    }
-    
-    //vitesse 
-    valeurAmelioration2.value = setInterval(() => {
-        cumul.value += valeurAutoClick.value;
-    }, 1000);
-}
 
 // Démarrer l'auto-click automatiquement si déjà acheté
 onMounted(() => {
